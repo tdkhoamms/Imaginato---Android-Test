1234d4a3fd90986ddb7f1af90307a8d90277de07
tdkhoa <tdkhoa@mmsofts.com>
2020-12-28 13:38:48 +0700

    Fix crash app

--- app/src/main/java/com/example/room/mvvm/view/MainActivity.kt
+++ app/src/main/java/com/example/room/mvvm/view/MainActivity.kt
@@ -8,13 +8,14 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.room.mvvm.R
 import com.example.room.mvvm.viewmodel.LoginViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var loginViewModel: LoginViewModel
-
     private lateinit var context: Context
-
     private lateinit var strUsername: String
     private lateinit var strPassword: String
 
@@ -37,14 +38,59 @@ class MainActivity : AppCompatActivity() {
         btnAddLogin.setOnClickListener {
             strUsername = txtUsername.text.toString().trim()
             strPassword = txtPassword.text.toString().trim()
-            if (strPassword.isEmpty()) {
-                txtUsername.error = "Please enter the username"
-            } else if (strPassword.isEmpty()) {
-                txtPassword.error = "Please enter the username"
-            } else {
-                loginViewModel.callApiLogin(context, strUsername, strPassword, lblInsertResponse)
+            when {
+                strPassword.isEmpty() -> {
+                    txtUsername.error = "Please enter the username"
+                }
+                strPassword.isEmpty() -> {
+                    txtPassword.error = "Please enter the username"
+                }
+                else -> {
+                    onLogin(context, strUsername, strPassword)
+                }
             }
         }
+
+
+    }
+
+    private fun onLogin(context: Context, strUsername: String, strPassword: String) {
+        val client = OkHttpClient()
+        val url = "https://private-222d3-homework5.apiary-mock.com/api/login"
+        val formBody = FormBody.Builder()
+            .add("username", strUsername)
+            .add("password", strPassword)
+            .build()
+
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("Content-Type", "application/json")
+            .addHeader("IMSI", "357175048449937")
+            .addHeader("IMEI", "510110406068589")
+            .post(formBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                runOnUiThread {
+                    lblInsertResponse.text = "Login Error"
+                }
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val x_Acc = response.header("X-Acc") ?: ""
+                val jsonData = response.body()!!.string()
+                val obj = JSONObject(jsonData)
+                val userObj = obj.getJSONObject("user")
+                val userId = userObj.getString("userId")
+                val userName = userObj.getString("userName")
+                loginViewModel.insertData(context, userName, strPassword, userId, x_Acc)
+                runOnUiThread {
+                    lblInsertResponse.text = "Login Successfully"
+                }
+            }
+        })
     }
 
 
--- app/src/main/java/com/example/room/mvvm/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/room/mvvm/viewmodel/LoginViewModel.kt
@@ -1,16 +1,11 @@
 package com.example.room.mvvm.viewmodel
 
 import android.content.Context
-import android.widget.TextView
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.room.mvvm.model.LoginTableModel
 import com.example.room.mvvm.repository.LoginRepository
-import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.*
-import org.json.JSONObject
-import java.io.IOException
+
 
 class LoginViewModel : ViewModel() {
 
@@ -29,46 +24,6 @@ class LoginViewModel : ViewModel() {
     fun getLoginDetails(context: Context): LiveData<LoginTableModel>? {
         liveDataLogin = LoginRepository.getLoginDetails(context)
         return liveDataLogin
-    }
-
-    fun callApiLogin(
-        context: Context,
-        strUsername: String,
-        strPassword: String,
-        lblInsertResponse: TextView
-    ) {
-        val client = OkHttpClient()
-        val url = "https://private-222d3-homework5.apiary-mock.com/api/login"
-        val formBody = FormBody.Builder()
-            .add("username", strUsername)
-            .add("password", strPassword)
-            .build()
-
-        val request = Request.Builder()
-            .url(url)
-            .addHeader("Content-Type", "application/json")
-            .addHeader("IMSI", "357175048449937")
-            .addHeader("IMEI", "510110406068589")
-            .post(formBody)
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                lblInsertResponse.text = "Login Error"
-                e.printStackTrace()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                val x_Acc = response.header("X-Acc") ?: ""
-                val jsonData = response.body()!!.string()
-                val obj = JSONObject(jsonData)
-                val userObj = obj.getJSONObject("user")
-                val userId = userObj.getString("userId")
-                val userName = userObj.getString("userName")
-                insertData(context, userName, strPassword, userId, x_Acc)
-                lblInsertResponse.text = "Login Successfully"
-            }
-        })
     }
 
 }
